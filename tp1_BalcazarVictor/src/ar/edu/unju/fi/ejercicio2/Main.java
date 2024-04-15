a1d87a66634f9b693644f08ef0c59f5faf55e068
victor <victor@LAPTOP-6G995AJF>
2024-04-15 16:16:07 -0300

    subo el ejercicio 2

---
+++ tp1_BalcazarVictor/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+
+import java.lang.Math;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		int edadPersona = 20;
+		double alturaEdificio = 30.5;
+		double precioProducto = 499.99;
+		String telefono = "3881234567";
+		double coseno = Math.cos(0.5);
+
+		System.out.println("Pais: "+nombrePais);
+		System.out.println("Edad: "+edadPersona +" años");
+		System.out.println("Altura: "+alturaEdificio+" metros");
+		System.out.println("Precio: $"+precioProducto);
+		System.out.println("telefono: "+telefono);
+		System.out.println("Coseno: "+coseno);
+	}
+
+}
