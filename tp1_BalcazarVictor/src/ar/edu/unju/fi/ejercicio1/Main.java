5b2286956685c15174c710d2af4f73f145b241d0
victor <victor@LAPTOP-6G995AJF>
2024-04-15 00:51:23 -0300

    subo el primer punto del tp

---
+++ tp1_BalcazarVictor/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			int numero1 = 8;
+			int numero2 = 5;
+			int numero3 = 1;
+			int numero4 = 20;
+			int numero5 = 9;
+			
+			double promedio = (double)(numero1 + numero2 + numero3 + numero4 + numero5) / 5;
+			
+			System.out.println("El promedio es: "+ promedio);
+			
+
+	}
+
+}
